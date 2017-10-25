c2f60be1baa3e6435e5242a56d7034d2ccacfe0b
jhaocheng <jhaocheng.wu@gmail.com>
2017-10-25 18:50:01 +0800

    Add zh-tw(Chinese(Traditional)) in lang/.

---
+++ lang/zh-tw.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt 卡片系統';
+
+$lang['foot_note'] = 'Ticket-Web Interface &copy; by TecArt GmbH';
+
+$lang['custom_number']  = '使用者 ID';
+$lang['password']       = '密碼';
+$lang['login']          = '登入';
+$lang['headline_standard'] = 'Ticket-Web Interface';
+$lang['headline']       = 'TecArt Ticket-Web Interface';
+$lang['logout']         = '登出';
+$lang['view']           = '瀏覽';
+
+$lang['show_current']       = '現在的卡片';
+$lang['show_all']           = '所有卡片';
+$lang['create_new']         = '建立新卡片';
+$lang['closed_ticket']      = '標記完成';
+$lang['not_handle_ticket']  = '取消卡片';
+$lang['partner_portal']     = '合作夥伴';
+$lang['partner_wishlist']   = '需求清單';
+
+$lang['show_all_year']      = '所有年份';
+$lang['show_all_month']     = '所有月份';
+
+$lang['activity']       = '活動';
+$lang['activity_type']  = '活動類別';
+$lang['name']           = '姓名';
+$lang['email']          = 'E-mail';
+$lang['call']           = '電話';
+$lang['action']         = '行動';
+$lang['category']       = '類別';
+$lang['priority']       = '優先順序';
+$lang['subject']        = '主題';
+$lang['description']    = '描述';
+$lang['save']           = '儲存';
+$lang['status']         = '狀態';
+$lang['id']             = '內部 ID';
+$lang['tnumber']        = '卡片 #';
+$lang['create_date']    = '建立日期';
+$lang['edit']           = '編輯';
+$lang['year']           = '年';
+$lang['month']          = '月';
+
+$lang['document']       = '文件';
+$lang['type']           = '檔案類別';
+$lang['size']           = '檔案大小';
+$lang['upload_date']    = '上傳日期';
+
+$lang['jan']            = '一月';
+$lang['feb']            = '二月';
+$lang['mar']            = '三月';
+$lang['apr']            = '四月';
+$lang['mai']            = '五月';
+$lang['jun']            = '六月';
+$lang['jul']            = '七月';
+$lang['aug']            = '八月';
+$lang['sep']            = '九月';
+$lang['oct']            = '十月';
+$lang['nov']            = '十一月';
+$lang['dec']            = '十二月';
+
+$lang['show_ticke_all']      = '顯示全部';
+$lang['show_ticket_actions'] = '顯示行動';
+$lang['show_ticke_emails']   = '顯示 Email';
+$lang['show_ticke_calls']    = '顯示通知';
+$lang['change_password']     = '更新密碼';
+$lang['old_password']        = '舊密碼';
+$lang['new_password']        = '新密碼';
+$lang['confirm_password']    = '確認新密碼';
+$lang['change']              = '確認更新';
+$lang['ticket_close']        = '完成這張卡片';
+
+$lang['post_reply']     = '建立行動';
+$lang['date']           = '日期';
+$lang['from']           = '從';
+$lang['too']            = '到';
+$lang['questioner']     = '提出者';
+$lang['content']        = '內容';
+$lang['duration']       = '期間';
+$lang['post']           = '文章';
+$lang['entries']        = '活動';
+$lang['last_entry']     = '最近一次活動';
+
+$lang['sections']          = '段落';
+$lang['tsection']          = '段落';
+$lang['show_all_sections'] = '顯示所有段落';
+
+$lang['today']          = '今天';
+$lang['yesterday']      = '昨天';
+$lang['nr']             = 'Nr.';
+$lang['company']        = '公司';
+$lang['file_upload']    = '上傳檔案';
+$lang['upload']         = '上傳';
+
+$lang['upload_folder_name']  = '上傳';
+
+$lang['err_wrong_password']          = '您的密碼輸入錯誤！';
+$lang['err_password_not_matched']    = '您的密碼和設定的不相符！';
+$lang['err_no_username_password']    = '請輸入您的帳號及密碼！';
+$lang['err_no_new_password']         = '請輸入新的密碼！';
+$lang['err_no_old_password']         = '請輸入您現在的密碼！';
+$lang['err_crm_connect']             = '無法連線至 TecArt-System.<br><br>請聯繫客服！';
+$lang['err_incorrect_cid']           = '您輸入的帳號有誤！';
+$lang['err_incorrect_username_pass'] = '您輸入的帳號/密碼有誤！';
+$lang['err_cid_not_found']           = '找不到聯絡人 ID<br><br>請重新登入！';
+$lang['err_tid_not_found']           = '找不到卡片 ID<br><br>請重新登入！';
+$lang['err_no_ticket_found']         = '找不到卡片！';
+$lang['err_no_contact_found']        = '找不到聯絡人！';
+$lang['err_no_document_found']       = '找不到文件！';
+$lang['err_no_name']                 = '請輸入您的姓名！';
+$lang['err_no_subject']              = '請輸入標題！';
+$lang['err_no_notes']                = '請輸入您的描述！';
+$lang['err_no_email']                = '請輸入您的 Email！';
+$lang['err_not_supported_email']     = '請輸入一個有效的 Email!';
+$lang['err_no_responsibler_found']   = '找不到內容 <br><br>請聯繫客服！';
+$lang['err_create_ticket']           = '建立卡片時發生錯誤<br><br>請再試一次！';
+$lang['err_create_ticket_action']    = '建立卡片行動時發生錯誤<br><br>請再試一次！';
+$lang['err_connection']              = '連線異常，請再試一次！';
+$lang['err_no_authorization']        = '您沒有權限讀取這張卡片。<br><br>請聯繫客服！';
+$lang['err_file_upload']             = '檔案上傳時發生錯誤，請再試一次！';
+$lang['err_adding_doc']              = '傳送文件至 TecArt-System 時發生錯誤<br><br>請再試一次！';
+$lang['err_create_folder']           = '建立資料夾時發生錯誤<br><br>請再試一次！';
+$lang['err_db']                      = '從 TecArt-System 取得文件時發生錯誤！';
+$lang['no_activity']                 = '您所選的活動並沒有內容！';
+$lang['no_set_activity']             = '請至少選擇一項活動！';
+$lang['err_filter_nomonth']          = '在選擇月份之前請先選擇年份';
+$lang['err_ok']                      = 'Ok';
+$lang['err_partners_not_enabled']    = '合作夥伴的功能還尚未開通。';
+$lang['err_partners_no_contract']    = '嘗試使用合作夥伴功能時發生錯誤 (可能是您沒有合作夥伴，或者沒有權限讀取)';
+
+$lang['create_ticket_success']          = '卡片建立成功！';
+$lang['create_ticket_action_success']   = '行動建立成功！';
+$lang['doc_upload_success']             = '文件成功上傳！';
+$lang['change_pass_success']            = '您的密碼已經更改成功！';
+$lang['change_pass_error']              = '更改密碼時發生錯誤！';
+
+$lang['section_standard'] = '標準';
+
+$lang['levelup']      = '升級';
+$lang['refresh']      = '重新整理';
+$lang['folder_empty'] = '這個資料夾是空的';
+$lang['mime_folder']  = '資料夾';
