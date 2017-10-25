d0e1e79c0b94cbd4e2e9a7fe41a4b7dcd76cc8f9
Gautam Mandsorwale <mandsorwalegautam@gmail.com>
2017-10-25 20:00:34 +0530

    Added French Translation strings

---
+++ lang/fr.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt Ticket System';
+
+$lang['foot_note'] = 'Ticket-Web Interface &copy; by TecArt GmbH';
+
+$lang['custom_number']  = 'N ° de client';
+$lang['password']       = 'Mot de passe';
+$lang['login']          = 'S\'identifier';
+$lang['headline_standard'] = 'Ticket-Web Interface';
+$lang['headline']       = 'TecArt Ticket-Web Interface';
+$lang['logout']         = 'Connectez - Out';
+$lang['view']           = 'Vue';
+
+$lang['show_current']       = 'Billets actuels';
+$lang['show_all']           = 'Tous les billets';
+$lang['create_new']         = 'Créer un nouveau ticket';
+$lang['closed_ticket']      = 'Billets fermés';
+$lang['not_handle_ticket']  = 'Billets non gérées';
+$lang['partner_portal']     = 'Portail des partenaires';
+$lang['partner_wishlist']   = 'Favoris';
+
+$lang['show_all_year']      = 'Toutes les années ...';
+$lang['show_all_month']     = 'Tous les mois ...';
+
+$lang['activity']       = 'Activité';
+$lang['activity_type']  = 'Type';
+$lang['name']           = 'prénom';
+$lang['email']          = 'Email';
+$lang['call']           = 'Appel';
+$lang['action']         = 'action';
+$lang['category']       = 'Catégorie';
+$lang['priority']       = 'Priorité';
+$lang['subject']        = 'Assujettir';
+$lang['description']    = 'La description';
+$lang['save']           = 'sauvegarder';
+$lang['status']         = 'Statut';
+$lang['id']             = 'ID interne';
+$lang['tnumber']        = 'Numéro de ticket';
+$lang['create_date']    = 'Date de création';
+$lang['edit']           = 'modifier';
+$lang['year']           = 'An';
+$lang['month']          = 'Mois';
+
+$lang['document']       = 'Document';
+$lang['type']           = 'Type de fichier';
+$lang['size']           = 'Taille du fichier';
+$lang['upload_date']    = 'Date de dépôt';
+
+$lang['jan']            = 'Janvier';
+$lang['feb']            = 'Février';
+$lang['mar']            = 'Mars';
+$lang['apr']            = 'Avril';
+$lang['mai']            = 'Mai';
+$lang['jun']            = 'Juin';
+$lang['jul']            = 'Juillet';
+$lang['aug']            = 'Août';
+$lang['sep']            = 'Septembre';
+$lang['oct']            = 'Octobre';
+$lang['nov']            = 'Novembre';
+$lang['dec']            = 'Décembre';
+
+$lang['show_ticke_all']      = 'Montre tout';
+$lang['show_ticket_actions'] = 'Afficher les actions';
+$lang['show_ticke_emails']   = 'Afficher les emails';
+$lang['show_ticke_calls']    = 'Afficher les appels';
+$lang['change_password']     = 'Mettre le mot de passe';
+$lang['old_password']        = 'ancien mot de passe';
+$lang['new_password']        = 'nouveau mot de passe';
+$lang['confirm_password']    = 'Confirmer le nouveau mot de passe';
+$lang['change']              = 'Mettre à jour';
+$lang['ticket_close']        = 'Fermer ce billet';
+
+$lang['post_reply']     = 'Créer une action';
+$lang['date']           = 'Rendez-vous amoureux';
+$lang['from']           = 'De';
+$lang['too']            = 'À';
+$lang['questioner']     = 'Questionneur';
+$lang['content']        = 'Content';
+$lang['duration']       = 'Durée';
+$lang['post']           = 'Poster';
+$lang['entries']        = 'Activité';
+$lang['last_entry']     = 'Dernière Activité';
+
+$lang['sections']          = 'Sections';
+$lang['tsection']          = 'Section';
+$lang['show_all_sections'] = 'Afficher toutes les sections';
+
+$lang['today']          = 'Aujourd\'hui';
+$lang['yesterday']      = 'Hier';
+$lang['nr']             = 'Nr.';
+$lang['company']        = 'Compagnie';
+$lang['file_upload']    = 'Téléchargement de fichiers';
+$lang['upload']         = 'Télécharger';
+
+$lang['upload_folder_name']  = 'Télécharger';
+
+$lang['err_wrong_password']          = 'Vous avez entré un mauvais mot de passe!';
+$lang['err_password_not_matched']    = 'Les mots de passe ne correspondent pas!';
+$lang['err_no_username_password']    = 'Veuillez s\'il vous plaît entrer votre nom d\'utilisateur et votre mot de passe!';
+$lang['err_no_new_password']         = 'S\'il vous plaît entrer un nouveau mot de passe!';
+$lang['err_no_old_password']         = 'Veuillez entrer votre mot de passe actuel!';
+$lang['err_crm_connect']             = 'Impossible de se connecter à TecArt-System. <br> <br> Veuillez contacter le support!';
+$lang['err_incorrect_cid']           = 'Vous avez entré un nom d\'utilisateur incorrect!';
+$lang['err_incorrect_username_pass'] = 'Le nom d\'utilisateur et / ou mot de passe fourni est invalide!';
+$lang['err_cid_not_found']           = 'Contact ID introuvable. <br> <br> Veuillez vous reconnecter!';
+$lang['err_tid_not_found']           = 'ID de ticket non trouvé. <br> <br> Veuillez vous reconnecter!';
+$lang['err_no_ticket_found']         = 'Aucun billet trouvé!';
+$lang['err_no_contact_found']        = 'Aucun contact trouvé';
+$lang['err_no_document_found']       = 'Aucun document trouvé!';
+$lang['err_no_name']                 = 'S\'il vous plaît entrez votre nom!';
+$lang['err_no_subject']              = 'S\'il vous plaît entrer un sujet!';
+$lang['err_no_notes']                = 'S\'il vous plaît entrer une description!';
+$lang['err_no_email']                = 'S\'il vous plaît informer votre e-mail!';
+$lang['err_not_supported_email']     = 'S\'il vous plaît informer une adresse email valide!';
+$lang['err_no_responsibler_found']   = 'Responsable non trouvé. <br> <br> Veuillez contacter le support!';
+$lang['err_create_ticket']           = 'Erreur lors de la création de Ticket! <br> <br> Veuillez réessayer!';
+$lang['err_create_ticket_action']    = 'Erreur lors de la création de l\'action Ticket! <br> <br> Veuillez réessayer!';
+$lang['err_connection']              = 'Erreur de connexion! Veuillez réessayer!';
+$lang['err_no_authorization']        = 'Vous n\'avez pas accès à ce ticket. <br> <br> Veuillez contacter le support!';
+$lang['err_file_upload']             = 'Erreur lors du téléchargement du fichier! Veuillez réessayer.';
+$lang['err_adding_doc']              = 'Erreur lors du transfert du document dans le système TecArt! <br> <br> Veuillez réessayer!';
+$lang['err_create_folder']           = 'Erreur lors de la création d\'un nouveau dossier. <br> <br> Veuillez réessayer!';
+$lang['err_db']                      = 'Erreur lors de la récupération du document de TecArt-System!';
+$lang['no_activity']                 = 'Il n\'y a pas de contenu pour les activités sélectionnées!';
+$lang['no_set_activity']             = 'Veuillez sélectionner au moins une activité!';
+$lang['err_filter_nomonth']          = 'Veuillez sélectionner l\'année avant le filtrage par mois.';
+$lang['err_ok']                      = 'D\'accord';
+$lang['err_partners_not_enabled']    = 'La fonction du portail partenaire n\'est pas active.';
+$lang['err_partners_no_contract']    = 'L\'accès au portail des partenaires a été refusé! (aucun contrat d\'accès partenaire actif ou accès désactivé)';
+
+$lang['create_ticket_success']          = 'Ticket créé avec succès.';
+$lang['create_ticket_action_success']   = 'Action envoyée avec succès';
+$lang['doc_upload_success']             = 'Document téléchargé avec succès.';
+$lang['change_pass_success']            = 'Le mot de passe a été modifié avec succès.';
+$lang['change_pass_error']              = 'Erreur lors de la mise à jour du mot de passe!';
+
+$lang['section_standard'] = 'la norme';
+
+$lang['levelup']      = 'Niveau supérieur';
+$lang['refresh']      = 'Rafraîchir';
+$lang['folder_empty'] = 'Ce dossier est vide.';
+$lang['mime_folder']  = 'Dossier';
