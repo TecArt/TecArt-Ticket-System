18ef0f296152fa36b3218895752bf89de890f63a
Slevin Dyachenko <slevin.dyachenko@gmail.com>
2017-10-25 22:21:37 +0300

    Add russian translation

---
+++ lang/ru.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt Ticket System';
+
+$lang['foot_note'] = 'Ticket-Web Interface &copy; by TecArt GmbH';
+
+$lang['custom_number']  = 'ID Пользователя';
+$lang['password']       = 'Пароль';
+$lang['login']          = 'Логин';
+$lang['headline_standard'] = 'Ticket-Web Interface';
+$lang['headline']       = 'TecArt Ticket-Web Interface';
+$lang['logout']         = 'Выход';
+$lang['view']           = 'Вид';
+
+$lang['show_current']       = 'Текущие Карточки';
+$lang['show_all']           = 'Все Карточки';
+$lang['create_new']         = 'Создать новую Карточку';
+$lang['closed_ticket']      = 'Закрытые Карточки';
+$lang['not_handle_ticket']  = 'Необработанные Карточки';
+$lang['partner_portal']     = 'Партнёрский Портал';
+$lang['partner_wishlist']   = 'Список желаний';
+
+$lang['show_all_year']      = 'Все Годы...';
+$lang['show_all_month']     = 'Все Месяцы...';
+
+$lang['activity']       = 'Активность';
+$lang['activity_type']  = 'Тип';
+$lang['name']           = 'Имя';
+$lang['email']          = 'Email';
+$lang['call']           = 'Звонок';
+$lang['action']         = 'Действие';
+$lang['category']       = 'Категория';
+$lang['priority']       = 'Приоритет';
+$lang['subject']        = 'Тема';
+$lang['description']    = 'Описание';
+$lang['save']           = 'Сохранить';
+$lang['status']         = 'Статус';
+$lang['id']             = 'Внутренний ID';
+$lang['tnumber']        = 'Карточка #';
+$lang['create_date']    = 'Дата Создания';
+$lang['edit']           = 'Редактировать';
+$lang['year']           = 'Год';
+$lang['month']          = 'Месяц';
+
+$lang['document']       = 'Документ';
+$lang['type']           = 'Тип файла';
+$lang['size']           = 'Размер файла';
+$lang['upload_date']    = 'Дата загрузки';
+
+$lang['jan']            = 'Январь';
+$lang['feb']            = 'Февраль';
+$lang['mar']            = 'Март';
+$lang['apr']            = 'Апрель';
+$lang['mai']            = 'Май';
+$lang['jun']            = 'Июнь';
+$lang['jul']            = 'Июль';
+$lang['aug']            = 'Август';
+$lang['sep']            = 'Сентябрь';
+$lang['oct']            = 'Октябрь';
+$lang['nov']            = 'Ноябрь';
+$lang['dec']            = 'Декабрь';
+
+$lang['show_ticke_all']      = 'Показать Все';
+$lang['show_ticket_actions'] = 'Показать Действия';
+$lang['show_ticke_emails']   = 'Показать Email-ы';
+$lang['show_ticke_calls']    = 'Показать Звонки';
+$lang['change_password']     = 'Обновить Пароль';
+$lang['old_password']        = 'Старый Пароль';
+$lang['new_password']        = 'Новый Пароль';
+$lang['confirm_password']    = 'Подтвердить Новый Пароль';
+$lang['change']              = 'Изменить';
+$lang['ticket_close']        = 'Закрыть эту Карточку';
+
+$lang['post_reply']     = 'Cоздать Действие';
+$lang['date']           = 'Дата';
+$lang['from']           = 'От';
+$lang['too']            = 'Кому';
+$lang['questioner']     = 'Интервьюер';
+$lang['content']        = 'Контент';
+$lang['duration']       = 'Продолжительность';
+$lang['post']           = 'Публикация';
+$lang['entries']        = 'Активность';
+$lang['last_entry']     = 'Последняя Активность';
+
+$lang['sections']          = 'Секции';
+$lang['tsection']          = 'Секция';
+$lang['show_all_sections'] = 'Показать все секции';
+
+$lang['today']          = 'Сегодня';
+$lang['yesterday']      = 'Вчера';
+$lang['nr']             = 'Nr.';
+$lang['company']        = 'Компания';
+$lang['file_upload']    = 'Загрузка Файла';
+$lang['upload']         = 'Загрузка';
+
+$lang['upload_folder_name']  = 'Загрузки';
+
+$lang['err_wrong_password']          = 'Вы ввели неверный пароль!';
+$lang['err_password_not_matched']    = 'Пароли не совпадают!';
+$lang['err_no_username_password']    = 'Пожалуйста, введите ваши имя пользователя и пароль!';
+$lang['err_no_new_password']         = 'Пожалуйста, введите новый пароль!';
+$lang['err_no_old_password']         = 'Пожалуйста, введите ваш текущий пароль!';
+$lang['err_crm_connect']             = 'Невозможно соединиться с TecArt-System.<br><br>Пожалуйста, обратитесь в службу поддержки!';
+$lang['err_incorrect_cid']           = 'Вы ввели неверное имя пользователя!';
+$lang['err_incorrect_username_pass'] = 'Имя пользователя и/или пароль неверны!';
+$lang['err_cid_not_found']           = 'ID контакта не найден.<br><br>Пожалуйста, войдите снова!';
+$lang['err_tid_not_found']           = 'ID карточки не найден.<br><br>Пожалуйста, войдите снова!';
+$lang['err_no_ticket_found']         = 'Карточки не найдены!';
+$lang['err_no_contact_found']        = 'Контакты не найдены!';
+$lang['err_no_document_found']       = 'Документы не найдены!';
+$lang['err_no_name']                 = 'Пожалуйста, введите своё имя!';
+$lang['err_no_subject']              = 'Пожалуйста, введите тему!';
+$lang['err_no_notes']                = 'Пожалуйста, введите описание!';
+$lang['err_no_email']                = 'Пожалуйста, укажите ваш email!';
+$lang['err_not_supported_email']     = 'Пожалуйста, укажите корректный email!';
+$lang['err_no_responsibler_found']   = 'Ответственный не найден.<br><br>Пожалуйста, обратитесь в службу поддержки!';
+$lang['err_create_ticket']           = 'Ошибка при создании Карточки!<br><br>Пожалуйста, попробуйте заново!';
+$lang['err_create_ticket_action']    = 'Ошибка при создании действия Карточки!<br><br>Пожалуйста, попробуйте заново!';
+$lang['err_connection']              = 'Ошибка подключения! Пожалуйста, попробуйте заново!';
+$lang['err_no_authorization']        = 'У вас нету доступа к этой карточке.<br><br>Пожалуйста, обратитесь в службу поддержки!';
+$lang['err_file_upload']             = 'Ошибка при загрузке файла! Пожалуйста, попробуйте заново!';
+$lang['err_adding_doc']              = 'Ошибка при передаче документа в TecArt-System!<br><br>Пожалуйста, попробуйте заново!';
+$lang['err_create_folder']           = 'Ошибка при создании новой папки!<br><br>Пожалуйста, попробуйте заново!';
+$lang['err_db']                      = 'Ошибка при получении документа от TecArt-System!';
+$lang['no_activity']                 = 'Отсутствует контент для выбранных активностей!';
+$lang['no_set_activity']             = 'Пожалуйста, выберите как минимум одну активность!';
+$lang['err_filter_nomonth']          = 'Пожалуйста, выберите год перед фильтрацией по месцам!';
+$lang['err_ok']                      = 'Ок';
+$lang['err_partners_not_enabled']    = 'Функция Партнёрского Портала не активна.';
+$lang['err_partners_no_contract']    = 'В доступе к Партнёрскому Порталу отказано! (отсутствует активный контракт доступа или доступ отключён)';
+
+$lang['create_ticket_success']          = 'Карточка успешно создана.';
+$lang['create_ticket_action_success']   = 'Действие успешно создано.';
+$lang['doc_upload_success']             = 'Документ успешно загружен.';
+$lang['change_pass_success']            = 'Пароль успешно изменён.';
+$lang['change_pass_error']              = 'Ошибка при обновлении пароля!';
+
+$lang['section_standard'] = 'Стандарт';
+
+$lang['levelup']      = 'Уровень выше';
+$lang['refresh']      = 'Обновить';
+$lang['folder_empty'] = 'Эта папка пуста.';
+$lang['mime_folder']  = 'Папка';
