c6db4581092c5929927c50679433c4788b5aa4db
Jakub Vyvážil <jakub@vyvazil.cz>
2017-10-29 18:33:39 +0100

    added czech translation

---
+++ lang/cs.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt Ticket System';
+
+$lang['foot_note'] = 'Ticket-Web Rozhraní &copy; od TecArt GmbH';
+
+$lang['custom_number']  = 'ID Zákazníka';
+$lang['password']       = 'Heslo';
+$lang['login']          = 'Login';
+$lang['headline_standard'] = 'Ticket-Web Rozhraní';
+$lang['headline']       = 'TecArt Ticket-Web Rozhraní';
+$lang['logout']         = 'Odhlásit';
+$lang['view']           = 'Zobrazit';
+
+$lang['show_current']       = 'Aktuální Tikety';
+$lang['show_all']           = 'Všechny Tikety';
+$lang['create_new']         = 'Vytvořit nový Tiket';
+$lang['closed_ticket']      = 'Uzavřené Tikety';
+$lang['not_handle_ticket']  = 'Nevyřešené Tikety';
+$lang['partner_portal']     = 'Partnerský Portál';
+$lang['partner_wishlist']   = 'Seznam Přání';
+
+$lang['show_all_year']      = 'Všechny Roky...';
+$lang['show_all_month']     = 'Všechny Měsíce...';
+
+$lang['activity']       = 'Aktivita';
+$lang['activity_type']  = 'Typ';
+$lang['name']           = 'Jméno';
+$lang['email']          = 'Email';
+$lang['call']           = 'Hovor';
+$lang['action']         = 'Akce';
+$lang['category']       = 'Kategorie';
+$lang['priority']       = 'Priorita';
+$lang['subject']        = 'Předmět';
+$lang['description']    = 'Popis';
+$lang['save']           = 'Uložit';
+$lang['status']         = 'Stav';
+$lang['id']             = 'Interní ID';
+$lang['tnumber']        = 'Tiket č.';
+$lang['create_date']    = 'Datum Vytvoření';
+$lang['edit']           = 'Editovat';
+$lang['year']           = 'Rok';
+$lang['month']          = 'Měsíc';
+
+$lang['document']       = 'Dokument';
+$lang['type']           = 'Typ Souboru';
+$lang['size']           = 'Velikost Souboru';
+$lang['upload_date']    = 'Datum Nahrání';
+
+$lang['jan']            = 'Leden';
+$lang['feb']            = 'Únor';
+$lang['mar']            = 'Březen';
+$lang['apr']            = 'Duben';
+$lang['mai']            = 'Květen';
+$lang['jun']            = 'Červen';
+$lang['jul']            = 'Červenes';
+$lang['aug']            = 'Srpen';
+$lang['sep']            = 'Září';
+$lang['oct']            = 'Říjen';
+$lang['nov']            = 'Listopad';
+$lang['dec']            = 'Prosinec';
+
+$lang['show_ticke_all']      = 'Zobrazit Vše';
+$lang['show_ticket_actions'] = 'Zobrazit Akce';
+$lang['show_ticke_emails']   = 'Zobrazit Emaily';
+$lang['show_ticke_calls']    = 'Zobrazit Hovory';
+$lang['change_password']     = 'Aktualizovat Heslo';
+$lang['old_password']        = 'Staré Heslo';
+$lang['new_password']        = 'Nové Heslo';
+$lang['confirm_password']    = 'Potvrdit Nové Heslo';
+$lang['change']              = 'Aktualizovat';
+$lang['ticket_close']        = 'Zavřít tento Tiket';
+
+$lang['post_reply']     = 'Vytvořit';
+$lang['date']           = 'Datum';
+$lang['from']           = 'Od';
+$lang['too']            = 'Do';
+$lang['questioner']     = 'Tazatel';
+$lang['content']        = 'Obsah';
+$lang['duration']       = 'Doba trvání';
+$lang['post']           = 'Příspěvek';
+$lang['entries']        = 'Aktivita';
+$lang['last_entry']     = 'Poslední Aktivita';
+
+$lang['sections']          = 'Sekce';
+$lang['tsection']          = 'Sekce';
+$lang['show_all_sections'] = 'Zobrazit všechny Sekce';
+
+$lang['today']          = 'Dnes';
+$lang['yesterday']      = 'Včera';
+$lang['nr']             = 'Č.';
+$lang['company']        = 'Společnost';
+$lang['file_upload']    = 'Nahrát Soubor';
+$lang['upload']         = 'Nahrát';
+
+$lang['upload_folder_name']  = 'Nahrát';
+
+$lang['err_wrong_password']          = 'Bylo vloženo špatné heslo!';
+$lang['err_password_not_matched']    = 'Hesla se neshodují!';
+$lang['err_no_username_password']    = 'Vložte uživatelské jméno a heslo!';
+$lang['err_no_new_password']         = 'Prosím, vložte nové heslo!';
+$lang['err_no_old_password']         = 'Prosím, vložte současné heslo!';
+$lang['err_crm_connect']             = 'Nelze se připojit k TecArt-System.<br><br>Prosím, kontaktujte Podporu!';
+$lang['err_incorrect_cid']           = 'Bylo vloženo nesprávné uživatelské jméno!';
+$lang['err_incorrect_username_pass'] = 'Vložené uživatelské jméno a/nebo heslo je chybné!';
+$lang['err_cid_not_found']           = 'ID Kontaktu nenalezeno.<br><br>Přihlašte se prosím znovu!';
+$lang['err_tid_not_found']           = 'ID Tiketu nenalezeno.<br><br>Přihlašte se prosím znovu!';
+$lang['err_no_ticket_found']         = 'Tikety nenalezeny!';
+$lang['err_no_contact_found']        = 'Kontakty nenalezeny!';
+$lang['err_no_document_found']       = 'Dokumenty nenalezeny!';
+$lang['err_no_name']                 = 'Vložte své jméno!';
+$lang['err_no_subject']              = 'Vložte předmět!';
+$lang['err_no_notes']                = 'Vložte popis!';
+$lang['err_no_email']                = 'Vložte email!';
+$lang['err_not_supported_email']     = 'Vložte validní emailovou adresu!';
+$lang['err_no_responsibler_found']   = 'Zodpovědná osoba nenalezena.<br><br>Prosím, kontaktujte Podporu!';
+$lang['err_create_ticket']           = 'Chyba při vytváření Tiketu!<br><br>Prosím, zkuste to znovu!';
+$lang['err_create_ticket_action']    = 'Chyba při vytváření akce Tiketu!<br><br>Prosím, zkuste to znovu!';
+$lang['err_connection']              = 'Chyba spojení! Prosím, zkuste to znovu!';
+$lang['err_no_authorization']        = 'K tomuto tiketu nemáte přístup.<br><br>Prosím, kontaktujte Podporu!';
+$lang['err_file_upload']             = 'Chyba při nahrávání Souboru! Prosím, zkuste to znovu.';
+$lang['err_adding_doc']              = 'Chyba při přenostu dokumentu do TecArt-System!<br><br>Prosím, zkuste to znovu!';
+$lang['err_create_folder']           = 'Chyba při vytváření složky!<br><br>Prosím, zkuste to znovu!';
+$lang['err_db']                      = 'Chyba při získávání dokumentu z TecArt-System!';
+$lang['no_activity']                 = 'Pro zvolené aktivity nebyl nalezen žádný obsah!';
+$lang['no_set_activity']             = 'Prosím, zvolte alespoň jednu aktivitu!';
+$lang['err_filter_nomonth']          = 'Prosím, vyberte rok než začnete filrovat podle měsíce.';
+$lang['err_ok']                      = 'Ok';
+$lang['err_partners_not_enabled']    = 'Funkce Partnerského Portálu není aktivní.';
+$lang['err_partners_no_contract']    = 'Přístup do Partnerského Portálu byl zamítnut! (neaktivní smlouva o přístupu nebo přístup zneaktivněn)';
+
+$lang['create_ticket_success']          = 'Tiket byl úspěšně vytvořen.';
+$lang['create_ticket_action_success']   = 'Akce byla úspěšně provedena.';
+$lang['doc_upload_success']             = 'Dokument byl úspěšně nahrán.';
+$lang['change_pass_success']            = 'Heslo bylo úspěšně změněno.';
+$lang['change_pass_error']              = 'Chyba při aktualizaci hesla!';
+
+$lang['section_standard'] = 'Standardní';
+
+$lang['levelup']      = 'Postoupit';
+$lang['refresh']      = 'Obnovit';
+$lang['folder_empty'] = 'Tato složka je prázdná.';
+$lang['mime_folder']  = 'Složka';
