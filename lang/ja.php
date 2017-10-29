22d69c730ed4963b88dcc3675529e7328ef5b9a4
Manh Dao Van <manhdaovan@gmail.com>
2017-10-29 09:36:25 +0900

    Add translation for Japanese

---
+++ lang/ja.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt Ticket System';
+
+$lang['foot_note'] = 'Ticket-Web Interface &copy; by TecArt GmbH';
+
+$lang['custom_number']  = 'お客さんのID';
+$lang['password']       = 'パスワード';
+$lang['login']          = 'ログイン';
+$lang['headline_standard'] = 'Ticket-Web Interface';
+$lang['headline']       = 'TecArt Ticket-Web Interface';
+$lang['logout']         = 'ログアウト';
+$lang['view']           = 'View';
+
+$lang['show_current']       = '現在のチケット';
+$lang['show_all']           = '全てのチケット';
+$lang['create_new']         = '新しいチケットを追加';
+$lang['closed_ticket']      = 'チケットを閉じる';
+$lang['not_handle_ticket']  = '未処理のチケット';
+$lang['partner_portal']     = 'パートナーポータル';
+$lang['partner_wishlist']   = 'ウィッシュリスト';
+
+$lang['show_all_year']      = '全ての年...';
+$lang['show_all_month']     = '全ての月...';
+
+$lang['activity']       = 'アクティビティ';
+$lang['activity_type']  = 'タイプ';
+$lang['name']           = '名刺';
+$lang['email']          = 'メール';
+$lang['call']           = '電話番号';
+$lang['action']         = '操作';
+$lang['category']       = 'カテゴリー';
+$lang['priority']       = '優先度';
+$lang['subject']        = 'Subject';
+$lang['description']    = '説明文';
+$lang['save']           = '保存';
+$lang['status']         = '状況';
+$lang['id']             = '内部のID';
+$lang['tnumber']        = 'チケット #';
+$lang['create_date']    = '追加日';
+$lang['edit']           = '編集';
+$lang['year']           = '年';
+$lang['month']          = '月';
+
+$lang['document']       = '書類';
+$lang['type']           = '書類のタイプ';
+$lang['size']           = '書類のサイズ';
+$lang['upload_date']    = 'アップロードした日';
+
+$lang['jan']            = '１月';
+$lang['feb']            = '２月';
+$lang['mar']            = '３月';
+$lang['apr']            = '４月';
+$lang['mai']            = '５月';
+$lang['jun']            = '６月';
+$lang['jul']            = '７月';
+$lang['aug']            = '８月';
+$lang['sep']            = '９月';
+$lang['oct']            = '１０月';
+$lang['nov']            = '１１月';
+$lang['dec']            = '１２月';
+
+$lang['show_ticke_all']      = 'すべて表示';
+$lang['show_ticket_actions'] = '操作を表示';
+$lang['show_ticke_emails']   = 'メールを表示';
+$lang['show_ticke_calls']    = '電話番号を表示';
+$lang['change_password']     = 'パスワードを変更';
+$lang['old_password']        = '旧パスワード';
+$lang['new_password']        = '新しいパスワード';
+$lang['confirm_password']    = '新しいパスワードを確認';
+$lang['change']              = '変更';
+$lang['ticket_close']        = 'チケットを閉じる';
+
+$lang['post_reply']     = '返事を作成';
+$lang['date']           = '日付';
+$lang['from']           = 'From';
+$lang['too']            = 'To';
+$lang['questioner']     = '質問者';
+$lang['content']        = '内容';
+$lang['duration']       = '期間';
+$lang['post']           = '投稿';
+$lang['entries']        = '活動';
+$lang['last_entry']     = '最後の活動';
+
+$lang['sections']          = 'セクション';
+$lang['tsection']          = 'セクション';
+$lang['show_all_sections'] = '全てのセクションを表示';
+
+$lang['today']          = '本日';
+$lang['yesterday']      = '翌日';
+$lang['nr']             = 'Nr.';
+$lang['company']        = '会社';
+$lang['file_upload']    = 'アップロードファイル';
+$lang['upload']         = 'アップロード';
+
+$lang['upload_folder_name']  = 'アップロード';
+
+$lang['err_wrong_password']          = 'パスワードが違います!';
+$lang['err_password_not_matched']    = 'パスワードとパスワード（確認）が一致しません!';
+$lang['err_no_username_password']    = 'ユーザー名とパスワードをご入力ください!';
+$lang['err_no_new_password']         = '新しいパスワードをご入力ください!';
+$lang['err_no_old_password']         = '現在のパスワードをご入力ください!';
+$lang['err_crm_connect']             = 'TecArt-Systemに接続できません.<br><br>サポーターをご連絡ください!';
+$lang['err_incorrect_cid']           = 'ユーザー名が違います!';
+$lang['err_incorrect_username_pass'] = 'ユーザー名又はパスワードが違います!';
+$lang['err_cid_not_found']           = 'コンタクトのIDが見つかりません.<br><br>もう一度ログインしてください!';
+$lang['err_tid_not_found']           = 'チケットのIDが見つかりません.<br><br>もう一度ログインしてください!';
+$lang['err_no_ticket_found']         = 'チケットが見つかりません!';
+$lang['err_no_contact_found']        = 'コンタクトが見つかりません!';
+$lang['err_no_document_found']       = '書類が見つかりません!';
+$lang['err_no_name']                 = '名刺をご入力ください!';
+$lang['err_no_subject']              = 'メールタイトルをご入力ください!';
+$lang['err_no_notes']                = '説明文をご入力ください!';
+$lang['err_no_email']                = 'メールをご入力ください!';
+$lang['err_not_supported_email']     = 'メールの正しくフォマットをご入力ください!';
+$lang['err_no_responsibler_found']   = '担当者が見つかりません.<br><br>サポーターをご連絡ください!';
+$lang['err_create_ticket']           = 'チケットが追加できません!<br><br>もう一度ご追加ください!';
+$lang['err_create_ticket_action']    = 'チケットの操作が追加できません!<br><br>もう一度ご追加ください!';
+$lang['err_connection']              = 'インタネットが接続できません!リロードしてください!';
+$lang['err_no_authorization']        = 'このチケットにアクセス権限がありません.<br><br>サポーターをご連絡ください!';
+$lang['err_file_upload']             = 'ファイルがアップロードできません! もう一度アップロドしてください.';
+$lang['err_adding_doc']              = '書類がTecArt-Systemに追加できません。!<br><br>もう一度追加してください!';
+$lang['err_create_folder']           = 'フォルダーを作成できません!<br><br>もう一度作成してください!';
+$lang['err_db']                      = 'Error retrieving the document from TecArt-System!';
+$lang['no_activity']                 = 'There are no content for the selected activities!';
+$lang['no_set_activity']             = 'Please select at least one activity!';
+$lang['err_filter_nomonth']          = 'Please select the year before filtering by month.';
+$lang['err_ok']                      = 'Ok';
+$lang['err_partners_not_enabled']    = 'The Partner Portal function is not active.';
+$lang['err_partners_no_contract']    = 'Access to the Partner Portal was denied! (no active partner access contract or access disabled)';
+
+$lang['create_ticket_success']          = 'チケットを作成しました。';
+$lang['create_ticket_action_success']   = 'Action sent successfully.';
+$lang['doc_upload_success']             = 'Document successfully uploaded.';
+$lang['change_pass_success']            = 'Password has been changed succesfully.';
+$lang['change_pass_error']              = 'Error while updating the password!';
+
+$lang['section_standard'] = 'Standard';
+
+$lang['levelup']      = 'Level Up';
+$lang['refresh']      = 'Refresh';
+$lang['folder_empty'] = 'This folder is empty.';
+$lang['mime_folder']  = 'Folder';
