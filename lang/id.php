84a36448201464770182e8e9f4260b8f5a42f51f
afif hendrawan <afif.hendrawan@gmail.com>
2017-10-30 18:00:35 +0700

    add translation to id

---
+++ lang/id.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt Ticket System';
+
+$lang['foot_note'] = 'Ticket-Tampilan Web &copy; by TecArt GmbH';
+
+$lang['custom_number']  = 'ID Pelanggan';
+$lang['password']       = 'Kata Sandi';
+$lang['login']          = 'Masuk';
+$lang['headline_standard'] = 'Ticket-Tampilan Web';
+$lang['headline']       = 'TecArt Ticket-Tampilan Web';
+$lang['logout']         = 'Logout';
+$lang['view']           = 'Tinjau';
+
+$lang['show_current']       = 'Tiket saat ini';
+$lang['show_all']           = 'Semua Tiket';
+$lang['create_new']         = 'Membuat Tiket Baru';
+$lang['closed_ticket']      = 'Tiket Ditutup';
+$lang['not_handle_ticket']  = 'Tiket Belum Tertangani';
+$lang['partner_portal']     = 'Portal Partner';
+$lang['partner_wishlist']   = 'Daftar Keinginan';
+
+$lang['show_all_year']      = 'Semua Tahun...';
+$lang['show_all_month']     = 'Semua Bulan...';
+
+$lang['activity']       = 'Aktivitas';
+$lang['activity_type']  = 'Tipe';
+$lang['name']           = 'Nama';
+$lang['email']          = 'E-mail';
+$lang['call']           = 'Panggilan';
+$lang['action']         = 'Aksi';
+$lang['category']       = 'Kategori';
+$lang['priority']       = 'Prioritas';
+$lang['subject']        = 'Judul';
+$lang['description']    = 'Deskripsi';
+$lang['save']           = 'Simpan';
+$lang['status']         = 'Status';
+$lang['id']             = 'ID Internal';
+$lang['tnumber']        = 'Tiket #';
+$lang['create_date']    = 'Tanggal Pembuatan';
+$lang['edit']           = 'Sunting';
+$lang['year']           = 'Tahun';
+$lang['month']          = 'Bulan';
+
+$lang['document']       = 'Documen';
+$lang['type']           = 'Tipe berkas';
+$lang['size']           = 'Ukuran berkas';
+$lang['upload_date']    = 'Tanggal unggal';
+
+$lang['jan']            = 'Januari';
+$lang['feb']            = 'Februari';
+$lang['mar']            = 'Maret';
+$lang['apr']            = 'April';
+$lang['mai']            = 'Mei';
+$lang['jun']            = 'Juni';
+$lang['jul']            = 'Juli';
+$lang['aug']            = 'Agustus';
+$lang['sep']            = 'September';
+$lang['oct']            = 'Oktober';
+$lang['nov']            = 'November';
+$lang['dec']            = 'Desember';
+
+$lang['show_ticke_all']      = 'Tampilkan Semua';
+$lang['show_ticket_actions'] = 'Tampilkan Semua';
+$lang['show_ticke_emails']   = 'Tampilkan Email';
+$lang['show_ticke_calls']    = 'Tampilkan Panggilan';
+$lang['change_password']     = 'Perbari Kata Sandi';
+$lang['old_password']        = 'Kata Sandi Lama';
+$lang['new_password']        = 'Kata Sandi Baru';
+$lang['confirm_password']    = 'Konfirmasi Kata Sandir Baru';
+$lang['change']              = 'Perbarui';
+$lang['ticket_close']        = 'Tutup tiket ini';
+
+$lang['post_reply']     = 'Membuat Aksi';
+$lang['date']           = 'Tanggal';
+$lang['from']           = 'Dari';
+$lang['too']            = 'Ke';
+$lang['questioner']     = 'Kuisioner';
+$lang['content']        = 'Konten';
+$lang['duration']       = 'Durasi';
+$lang['post']           = 'Pos';
+$lang['entries']        = 'Aktivitas';
+$lang['last_entry']     = 'Aktivitas Terakhir';
+
+$lang['sections']          = 'Bagian';
+$lang['tsection']          = 'Bagian';
+$lang['show_all_sections'] = 'Tampilkan Semua Bagian';
+
+$lang['today']          = 'Hari ini';
+$lang['yesterday']      = 'Kemarin';
+$lang['nr']             = 'Nr.';
+$lang['company']        = 'Perusahaan';
+$lang['file_upload']    = 'Unggah Berkas';
+$lang['upload']         = 'Unggah';
+
+$lang['upload_folder_name']  = 'Unggah';
+
+$lang['err_wrong_password']          = 'Anda memasukkan kata sandi yang salah!';
+$lang['err_password_not_matched']    = 'Kata sandi tidak cocok!';
+$lang['err_no_username_password']    = 'Masukkan nama pengguna dan kata sandi!';
+$lang['err_no_new_password']         = 'Silahkan masukkan kata sandi baru Anda!';
+$lang['err_no_old_password']         = 'Silahkan masukkan kata sandi Anda yang sekarang!';
+$lang['err_crm_connect']             = 'Tidak dapat terhubung ke TecArt-System.<br><br>Silahkan hubungi bagian bantuan!';
+$lang['err_incorrect_cid']           = 'Anda memasukkan nama pengguna yang salah!';
+$lang['err_incorrect_username_pass'] = 'Nama pengguna dan/atau kata sandi yang dimasukkan tidak valid!';
+$lang['err_cid_not_found']           = 'ID Kontak tidak ditemukan.<br><br>Silahkan masuk kembali!';
+$lang['err_tid_not_found']           = 'ID Tiket tidak ditemukan.<br><br>Silahkan masuk kembali!';
+$lang['err_no_ticket_found']         = 'Tiket tidak ditemukan!';
+$lang['err_no_contact_found']        = 'Tidak ada kontak ditemukan!';
+$lang['err_no_document_found']       = 'Tidak ada dokumen ditemukan!';
+$lang['err_no_name']                 = 'Silahkan masukkan nama Anda!';
+$lang['err_no_subject']              = 'Silahkkan masukkan judul!';
+$lang['err_no_notes']                = 'Silahkan masuukan deskripsi!';
+$lang['err_no_email']                = 'Silahkan informasikan email Anda!';
+$lang['err_not_supported_email']     = 'Silahkan informasikan email Anda yang valid!';
+$lang['err_no_responsibler_found']   = 'Penerima tidak ditemukan.<br><br>Silahkan hubungi bagian bantuan!';
+$lang['err_create_ticket']           = 'Gagal membuat tiket!<br><br>Silahkan coba lagi!';
+$lang['err_create_ticket_action']    = 'Gagal membuat aksi tiket!<br><br>Silahkan coba lagi!';
+$lang['err_connection']              = 'Koneksi terganggung! Silahkan coba lagi!';
+$lang['err_no_authorization']        = 'Anda tidak memiliki akses untuk tiket ini.<br><br>Silahkan hubungi bagian bantuan!';
+$lang['err_file_upload']             = 'Terjadi kesalahan pada saat proses pengunggahan! Silahkan coba lagi.';
+$lang['err_adding_doc']              = 'Terjadi kesalahan saat memindahkan dokumen ke the TecArt-System!<br><br>Silahkan coba lagi!';
+$lang['err_create_folder']           = 'Gagal membuat folder!<br><br>Silahkan coba lagi!';
+$lang['err_db']                      = 'Gagal menerima dokumen dari TecArt-System!';
+$lang['no_activity']                 = 'Tidak ada konten untuk aktivitas yang dipilih!';
+$lang['no_set_activity']             = 'Silahkan pilih paling tidak satu aktivitas!';
+$lang['err_filter_nomonth']          = 'Silahkan pilih tahun sebelum menyaring bulan.';
+$lang['err_ok']                      = 'Oke';
+$lang['err_partners_not_enabled']    = 'Portal Partner tidak aktif.';
+$lang['err_partners_no_contract']    = 'Akses ke Portal Partner ditolah! (tidak ada partner aktif mengakses kontrak atau akses dinonatifkan)';
+
+$lang['create_ticket_success']          = 'Tiket berhasil dibuat.';
+$lang['create_ticket_action_success']   = 'Aksi berhasil dikirim.';
+$lang['doc_upload_success']             = 'Dokumen berhasil diunggah.';
+$lang['change_pass_success']            = 'Kata sandi telah berhasil diubah.';
+$lang['change_pass_error']              = 'Terjadi kesalahan saat memperbarui kata sandi!';
+
+$lang['section_standard'] = 'Standard';
+
+$lang['levelup']      = 'Naik Tingkat';
+$lang['refresh']      = 'Perbarui';
+$lang['folder_empty'] = 'Folder ini kosong.';
+$lang['mime_folder']  = 'Folder';
