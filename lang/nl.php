3f78dda38b1943873cf52ed0f990621850cd17bd
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2019-10-02 12:32:08 +0200

    Added dutch (nl) language for TecArt Ticket System

---
+++ lang/nl.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'TecArt Ticket Systeem';
+
+$lang['foot_note'] = 'Ticket-Web Interface &copy; by TecArt GmbH';
+
+$lang['custom_number']  = 'Klant ID';
+$lang['password']       = 'Wachtwoord';
+$lang['login']          = 'Login';
+$lang['headline_standard'] = 'Ticket-Web Interface';
+$lang['headline']       = 'TecArt Ticket-Web Interface';
+$lang['logout']         = 'Uitloggen';
+$lang['view']           = 'Bekijken';
+
+$lang['show_current']       = 'Huidige Tickets';
+$lang['show_all']           = 'Alle Tickets';
+$lang['create_new']         = 'Maak een nieuw Ticket';
+$lang['closed_ticket']      = 'Sluit Ticket';
+$lang['not_handle_ticket']  = 'Onverwerkte Tickets';
+$lang['partner_portal']     = 'Partner Portaal';
+$lang['partner_wishlist']   = 'Wensenlijst';
+
+$lang['show_all_year']      = 'Alle Jaren...';
+$lang['show_all_month']     = 'Alle Maanden...';
+
+$lang['activity']       = 'Activiteit';
+$lang['activity_type']  = 'Type';
+$lang['name']           = 'Naam';
+$lang['email']          = 'E-mail';
+$lang['call']           = 'Bell';
+$lang['action']         = 'Actie';
+$lang['category']       = 'Categorie';
+$lang['priority']       = 'Prioriteit';
+$lang['subject']        = 'Onderwerp';
+$lang['description']    = 'Beschrijving';
+$lang['save']           = 'Opslaan';
+$lang['status']         = 'Status';
+$lang['id']             = 'Intern ID';
+$lang['tnumber']        = 'Ticket #';
+$lang['create_date']    = 'Aanmaakdatum';
+$lang['edit']           = 'Bewerken';
+$lang['year']           = 'Jaar';
+$lang['month']          = 'Maand';
+
+$lang['document']       = 'Document';
+$lang['type']           = 'Bestandstype';
+$lang['size']           = 'Bestandsgrootte';
+$lang['upload_date']    = 'Upload datum';
+
+$lang['jan']            = 'Januari';
+$lang['feb']            = 'Februari';
+$lang['mar']            = 'Maart';
+$lang['apr']            = 'April';
+$lang['mai']            = 'Mei';
+$lang['jun']            = 'Juni';
+$lang['jul']            = 'Juli';
+$lang['aug']            = 'Augustus';
+$lang['sep']            = 'September';
+$lang['oct']            = 'Oktober';
+$lang['nov']            = 'November';
+$lang['dec']            = 'December';
+
+$lang['show_ticke_all']      = 'Bekijk Alles';
+$lang['show_ticket_actions'] = 'Acties Tonen';
+$lang['show_ticke_emails']   = 'Emails Tonen';
+$lang['show_ticke_calls']    = 'Toon oproepen';
+$lang['change_password']     = 'Vernieuw wachtwoord';
+$lang['old_password']        = 'Oude wachtwoord';
+$lang['new_password']        = 'Nieuwe wachtwoord';
+$lang['confirm_password']    = 'Bevestig nieuw wachtwoord';
+$lang['change']              = 'Bijwerken';
+$lang['ticket_close']        = 'Sluit dit ticket';
+
+$lang['post_reply']     = 'Maak Actie';
+$lang['date']           = 'Datum';
+$lang['from']           = 'Van';
+$lang['to']             = 'Aan';
+$lang['questioner']     = 'Ondervrager';
+$lang['content']        = 'Inhoud';
+$lang['duration']       = 'Lengte';
+$lang['post']           = 'Post';
+$lang['entries']        = 'Activiteit';
+$lang['last_entry']     = 'Laatste Activiteit';
+
+$lang['sections']          = 'Secties';
+$lang['tsection']          = 'Sectie';
+$lang['show_all_sections'] = 'Toon alle secties';
+
+$lang['today']          = 'Vandaag';
+$lang['yesterday']      = 'Gisteren';
+$lang['nr']             = 'Nr.';
+$lang['company']        = 'Bedrijf';
+$lang['file_upload']    = 'Bestand Upload';
+$lang['upload']         = 'Upload';
+
+$lang['upload_folder_name']  = 'Upload';
+
+$lang['err_wrong_password']          = 'U heeft een verkeerd wachtwoord ingevoerd!';
+$lang['err_password_not_matched']    = 'Wachtwoorden komen niet overeen!';
+$lang['err_no_username_password']    = 'Voer uw gebruikersnaam en wachtwoord in!';
+$lang['err_no_new_password']         = 'Voer een nieuw wachtwoord in!';
+$lang['err_no_old_password']         = 'Voer je huidige wachtwoord in!';
+$lang['err_crm_connect']             = 'Kan geen verbinding maken met TecArt-System.<br><br>Neem contact op met support!';
+$lang['err_incorrect_cid']           = 'U heeft een onjuiste gebruikersnaam ingevoerd!';
+$lang['err_incorrect_username_pass'] = 'De opgegeven gebruikersnaam en / of wachtwoord is ongeldig!';
+$lang['err_cid_not_found']           = 'Contact-ID niet gevonden.<br><br>Log alsjeblieft nogmaals in!';
+$lang['err_tid_not_found']           = 'Ticket-ID niet gevonden.<br><br>Log alsjeblieft nogmaals in!';
+$lang['err_no_ticket_found']         = 'Geen tickets gevonden!';
+$lang['err_no_contact_found']        = 'Geen contacten gevonden!';
+$lang['err_no_document_found']       = 'Geen documenten gevonden!';
+$lang['err_no_name']                 = 'Voer je naam in!';
+$lang['err_no_subject']              = 'Voer een onderwerp in!';
+$lang['err_no_notes']                = 'Voer een omschrijving in!';
+$lang['err_no_email']                = 'Voer een email in!';
+$lang['err_not_supported_email']     = 'Vul alstublieft een geldig e-mailadres in!';
+$lang['err_no_responsibler_found']   = 'Verantwoordelijke niet gevonden.<br><br>Neem contact op met ondersteuning!';
+$lang['err_create_ticket']           = 'Fout bij het maken van het Ticket!<br><br>Probeer het opnieuw!';
+$lang['err_create_ticket_action']    = 'Fout bij het maken van de Ticket-actie!<br><br>Probeer het opnieuw!';
+$lang['err_connection']              = 'Verbindingsfout! Probeer het opnieuw!';
+$lang['err_no_authorization']        = 'U hebt geen toegang tot dit ticket.<br><br>Neem contact op met ondersteuning!';
+$lang['err_file_upload']             = 'Fout bij uploaden van bestand! Probeer het opnieuw.';
+$lang['err_adding_doc']              = 'Fout bij het overbrengen van het document naar het TecArt-systeem!<br><br>Probeer het opnieuw!';
+$lang['err_create_folder']           = 'Fout bij het maken van een nieuwe map!<br><br>Probeer het opnieuw!';
+$lang['err_db']                      = 'Fout bij het ophalen van het document van TecArt-System!';
+$lang['no_activity']                 = 'Er is geen inhoud voor de geselecteerde activiteiten!';
+$lang['no_set_activity']             = 'Selecteer ten minste één activiteit!';
+$lang['err_filter_nomonth']          = 'Selecteer het jaar voordat u op maand filtert.';
+$lang['err_ok']                      = 'Ok';
+$lang['err_partners_not_enabled']    = 'De Partner Portal-functie is niet actief.';
+$lang['err_partners_no_contract']    = 'Toegang tot het partnerportal is geweigerd! (geen actief partnertoegangscontract of de toegang is uitgeschakeld)';
+
+$lang['create_ticket_success']          = 'Ticket succesvol aangemaakt.';
+$lang['create_ticket_action_success']   = 'Actie succesvol verzonden.';
+$lang['doc_upload_success']             = 'Document succesvol geüpload.';
+$lang['change_pass_success']            = 'Wachtwoord is succesvol gewijzigd.';
+$lang['change_pass_error']              = 'Fout tijdens het bijwerken van het wachtwoord!';
+
+$lang['section_standard'] = 'Standaard';
+
+$lang['levelup']      = 'Niveau omhoog';
+$lang['refresh']      = 'Ververs';
+$lang['folder_empty'] = 'Deze map is leeg.';
+$lang['mime_folder']  = 'Map';
