881e83ef281c54c97ad3db137263735159c0b8dc
Đăng Tú <6233777-ngdangtu@users.noreply.gitlab.com>
2020-10-03 02:17:36 +0700

    Init vi version (incompleted)

---
+++ lang/vi.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'Hệ thống Thẻ TecArt';
+
+$lang['foot_note'] = 'Giao diện Web Thẻ &copy; bởi TecArt GmbH';
+
+$lang['custom_number']  = 'ID khách hàng';
+$lang['password']       = 'Mật khẩu';
+$lang['login']          = 'Đăng nhập';
+$lang['headline_standard'] = 'Giao diện Web Thẻ';
+$lang['headline']       = 'Giao diện Web Thẻ TecArt';
+$lang['logout']         = 'Đăng xuất';
+$lang['view']           = 'Xem';
+
+$lang['show_current']       = 'Current Tickets';
+$lang['show_all']           = 'All Tickets';
+$lang['create_new']         = 'Viết Thẻ mới';
+$lang['closed_ticket']      = 'Cất Thẻ';
+$lang['not_handle_ticket']  = 'Thẻ chưa xử lý';
+$lang['partner_portal']     = 'Cổng Đối tác';
+$lang['partner_wishlist']   = 'Sổ theo dõi';
+
+$lang['show_all_year']      = 'Mọi Năm...';
+$lang['show_all_month']     = 'Mọi Tháng...';
+
+$lang['activity']       = 'Hoạt động';
+$lang['activity_type']  = 'Loại';
+$lang['name']           = 'Tên';
+$lang['email']          = 'Email';
+$lang['call']           = 'Gọi';
+$lang['action']         = 'Tác vụ';
+$lang['category']       = 'Danh mục';
+$lang['priority']       = 'Ưu tiên';
+$lang['subject']        = 'Chủ đề';
+$lang['description']    = 'Miêu tả';
+$lang['save']           = 'Lưu';
+$lang['status']         = 'Trạng thái';
+$lang['id']             = 'ID nội bộ';
+$lang['tnumber']        = 'Số thẻ';
+$lang['create_date']    = 'Ngày tạo';
+$lang['edit']           = 'Sửa';
+$lang['year']           = 'Năm';
+$lang['month']          = 'Tháng';
+
+$lang['document']       = 'Tài liệu';
+$lang['type']           = 'Loại tập tin';
+$lang['size']           = 'Kích cỡ tập tin';
+$lang['upload_date']    = 'Ngày tải lên';
+
+$lang['jan']            = 'tháng Giêng';
+$lang['feb']            = 'tháng Hai';
+$lang['mar']            = 'tháng Ba';
+$lang['apr']            = 'tháng Tư';
+$lang['mai']            = 'tháng Năm';
+$lang['jun']            = 'tháng Sáu';
+$lang['jul']            = 'tháng Bảy';
+$lang['aug']            = 'tháng Tám';
+$lang['sep']            = 'tháng Chín';
+$lang['oct']            = 'tháng Mười';
+$lang['nov']            = 'tháng Mười Một';
+$lang['dec']            = 'tháng Chạp';
+
+$lang['show_ticke_all']      = 'Show All';
+$lang['show_ticket_actions'] = 'Hiện tác vụ';
+$lang['show_ticke_emails']   = 'Hiện Email';
+$lang['show_ticke_calls']    = 'Hiện cuộc gọi';
+$lang['change_password']     = 'Đổi mật khẩu';
+$lang['old_password']        = 'Mật khẩu cũ';
+$lang['new_password']        = 'Mật khẩu mới';
+$lang['confirm_password']    = 'Xác nhận mật khẩu mới';
+$lang['change']              = 'Cập nhật';
+$lang['ticket_close']        = 'Đóng Thẻ này';
+
+$lang['post_reply']     = 'Create Action';
+$lang['date']           = 'Ngày';
+$lang['from']           = 'Từ';
+$lang['to']             = 'Tới';
+$lang['questioner']     = 'Người hỏi';
+$lang['content']        = 'Nội dung';
+$lang['duration']       = 'Duration';
+$lang['post']           = 'Bài đăng';
+$lang['entries']        = 'Hoạt động';
+$lang['last_entry']     = 'Hoạt động cuối';
+
+$lang['sections']          = 'Phần';
+$lang['tsection']          = 'Phần';
+$lang['show_all_sections'] = 'Hiện tất cả các phần';
+
+$lang['today']          = 'Hôm nay';
+$lang['yesterday']      = 'Hôm qua';
+$lang['nr']             = 'Số ';
+$lang['company']        = 'Công ty';
+$lang['file_upload']    = 'Tải tập tin';
+$lang['upload']         = 'Tải lên';
+
+$lang['upload_folder_name']  = 'Upload';
+
+$lang['err_wrong_password']          = 'You entered a wrong password!';
+$lang['err_password_not_matched']    = 'Passwords do not match!';
+$lang['err_no_username_password']    = 'Please enter your username and password!';
+$lang['err_no_new_password']         = 'Please enter a new password!';
+$lang['err_no_old_password']         = 'Please enter your current password!';
+$lang['err_crm_connect']             = 'Can not connect to TecArt-System.<br><br>Please contact Support!';
+$lang['err_incorrect_cid']           = 'You have entered an incorrect username!';
+$lang['err_incorrect_username_pass'] = 'The provided username and/or password is invalid!';
+$lang['err_cid_not_found']           = 'Contact ID not found.<br><br>Please log in again!';
+$lang['err_tid_not_found']           = 'Ticket ID not found.<br><br>Please log in again!';
+$lang['err_no_ticket_found']         = 'No tickets found!';
+$lang['err_no_contact_found']        = 'Không tìm thấy liên hệ!';
+$lang['err_no_document_found']       = 'Không tìm thấy tài liệu!';
+$lang['err_no_name']                 = 'Xin mời bạn điền tên!';
+$lang['err_no_subject']              = 'Please enter a subject!';
+$lang['err_no_notes']                = 'Please enter a description!';
+$lang['err_no_email']                = 'Please inform your email!';
+$lang['err_not_supported_email']     = 'Please inform a valid e-mail adress!';
+$lang['err_no_responsibler_found']   = 'Responsible not found.<br><br>Please contact Support!';
+$lang['err_create_ticket']           = 'Error creating Ticket!<br><br>Please, try again!';
+$lang['err_create_ticket_action']    = 'Error creating Ticket action!<br><br>Please, try again!';
+$lang['err_connection']              = 'Connection error! Please, try again!';
+$lang['err_no_authorization']        = 'You do not have access to this ticket.<br><br>Please contact Support!';
+$lang['err_file_upload']             = 'Error on File Upload! Please, try again.';
+$lang['err_adding_doc']              = 'Error while transfering the document to the TecArt-System!<br><br>Please, try again!';
+$lang['err_create_folder']           = 'Error creating a new folder!<br><br>Please, try again!';
+$lang['err_db']                      = 'Error retrieving the document from TecArt-System!';
+$lang['no_activity']                 = 'There are no content for the selected activities!';
+$lang['no_set_activity']             = 'Please select at least one activity!';
+$lang['err_filter_nomonth']          = 'Please select the year before filtering by month.';
+$lang['err_ok']                      = 'Đồng ý';
+$lang['err_partners_not_enabled']    = 'The Partner Portal function is not active.';
+$lang['err_partners_no_contract']    = 'Access to the Partner Portal was denied! (no active partner access contract or access disabled)';
+
+$lang['create_ticket_success']          = 'Ticket successfully created.';
+$lang['create_ticket_action_success']   = 'Action sent successfully.';
+$lang['doc_upload_success']             = 'Tài liệu đã tải lên thành công.';
+$lang['change_pass_success']            = 'Đã thay đổi mật khẩu thành công.';
+$lang['change_pass_error']              = 'Lỗi xảy ra trong lúc đổi mật khẩu!';
+
+$lang['section_standard'] = 'Standard';
+
+$lang['levelup']      = 'Lên hạng';
+$lang['refresh']      = 'Làm mới';
+$lang['folder_empty'] = 'Thư mục này rỗng.';
+$lang['mime_folder']  = 'Thư mục';
