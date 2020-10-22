1bdf81a2969d60addf3d3d21645c978baa41e66f
mankal111 <mankal111@hotmail.com>
2020-10-22 16:52:31 +0300

    greek translation

---
+++ lang/el.php
@@ -0,0 +1,143 @@
+<?php
+
+$lang['title'] = 'Σύστημα εισητηρίων TecArt';
+
+$lang['foot_note'] = 'Ticket - Διαδικτυακή Διεπαφή &copy; από την TecArt GmbH';
+
+$lang['custom_number']  = 'ID Πελάτη';
+$lang['password']       = 'Κωδικός';
+$lang['login']          = 'Σύνδεση';
+$lang['headline_standard'] = 'Ticket - Διαδικτυακή Διεπαφή';
+$lang['headline']       = 'TecArt Ticket - Διαδικτυακή Διεπαφή';
+$lang['logout']         = 'Αποσύνδεση';
+$lang['view']           = 'Προβολή';
+
+$lang['show_current']       = 'Τρέχοντα εισητήρια';
+$lang['show_all']           = 'Όλα τα εισητήρια';
+$lang['create_new']         = 'Δημιουργία νέου εισητηρίου';
+$lang['closed_ticket']      = 'Κλειστά εισητήρια';
+$lang['not_handle_ticket']  = 'Ανεπίλυτα εισητήρια';
+$lang['partner_portal']     = 'Πύλη συνεργατών';
+$lang['partner_wishlist']   = 'Λίστα επιθυμιών';
+
+$lang['show_all_year']      = 'Όλα τα έτη...';
+$lang['show_all_month']     = 'Όλοι οι μήνες...';
+
+$lang['activity']       = 'Δραστηριότητα';
+$lang['activity_type']  = 'Τύπος';
+$lang['name']           = 'Όνομα';
+$lang['email']          = 'Ηλεκτρονικό ταχυδρομίο';
+$lang['call']           = 'Κλήση';
+$lang['action']         = 'Ενέργεια';
+$lang['category']       = 'Κατηγορία';
+$lang['priority']       = 'Προτεραιότητα';
+$lang['subject']        = 'Θέμα';
+$lang['description']    = 'Περιγραφή';
+$lang['save']           = 'Αποθήκευση';
+$lang['status']         = 'Κατάσταση';
+$lang['id']             = 'Εσωτερικό ID';
+$lang['tnumber']        = 'Εισητήριο #';
+$lang['create_date']    = 'Ημερομηνία δημιουργίας';
+$lang['edit']           = 'Επεξεργασία';
+$lang['year']           = 'Έτος';
+$lang['month']          = 'Μήνας';
+
+$lang['document']       = 'Έγγραφο';
+$lang['type']           = 'Τύπος αρχείου';
+$lang['size']           = 'Μέγεθος αρχείου';
+$lang['upload_date']    = 'Ημερομηνία μεταφόρτωσης';
+
+$lang['jan']            = 'Ιανουάριος';
+$lang['feb']            = 'Φεβρουάριος';
+$lang['mar']            = 'Μάρτιος';
+$lang['apr']            = 'Απρίλιος';
+$lang['mai']            = 'Μάιος';
+$lang['jun']            = 'Ιούνιος';
+$lang['jul']            = 'Ιούλιος';
+$lang['aug']            = 'Αύγουστος';
+$lang['sep']            = 'Σεπτέμβριος';
+$lang['oct']            = 'Οκτώβριος';
+$lang['nov']            = 'Νοέμβριος';
+$lang['dec']            = 'Δεκέμβριος';
+
+$lang['show_ticke_all']      = 'Προβολή όλων';
+$lang['show_ticket_actions'] = 'Προβολή ενεργειών';
+$lang['show_ticke_emails']   = 'Προβολή emails';
+$lang['show_ticke_calls']    = 'Προβολή κλήσεων';
+$lang['change_password']     = 'Επικαιροποίηση κωδικού';
+$lang['old_password']        = 'Παλιός κωδικός';
+$lang['new_password']        = 'Νέος κωδικός';
+$lang['confirm_password']    = 'Επιβεβαίωση νέου κωδικού';
+$lang['change']              = 'Επικαιροποίηση';
+$lang['ticket_close']        = 'Κλήσε αυτό το εισητήριο';
+
+$lang['post_reply']     = 'Δημιουργία ενέργειας';
+$lang['date']           = 'Ημερομηνία';
+$lang['from']           = 'Από';
+$lang['to']             = 'Πρός';
+$lang['questioner']     = 'Ερωτών';
+$lang['content']        = 'Περιεχόμενο';
+$lang['duration']       = 'Διάρκεια';
+$lang['post']           = 'Αποστολή';
+$lang['entries']        = 'Δραστηριότητα';
+$lang['last_entry']     = 'Τελευταία δραστηριότητα';
+
+$lang['sections']          = 'Ενότητες';
+$lang['tsection']          = 'Ενότητα';
+$lang['show_all_sections'] = 'Προβολή όλων των ενοτήτων';
+
+$lang['today']          = 'Σήμερα';
+$lang['yesterday']      = 'Χθές';
+$lang['nr']             = 'Αρ.';
+$lang['company']        = 'Εταιρία';
+$lang['file_upload']    = 'Μεταφόρτωση αρχείου';
+$lang['upload']         = 'Μεταφόρτωση';
+
+$lang['upload_folder_name']  = 'Μεταφόρτωση';
+
+$lang['err_wrong_password']          = 'Εισάγατε έναν λάθος κωδικό!';
+$lang['err_password_not_matched']    = 'Οι κωδικοί είναι διαφορετικοί μεταξύ τους!';
+$lang['err_no_username_password']    = 'Παρακαλώ γράψτε το όνομα χρήστη και τον κωδικό!';
+$lang['err_no_new_password']         = 'Παρακαλώ γράψτε έναν νέο κωδικό!';
+$lang['err_no_old_password']         = 'Παρακαλώ γράψτε τον τρέχοντα κωδικό σας!';
+$lang['err_crm_connect']             = 'Δεν μπορείτε να συνδεθείτε στο σύστημα TecArt.<br><br>Παρακαλώ επικοινωνήστε με την υποστήριξη πελατών!';
+$lang['err_incorrect_cid']           = 'Εισάγατε ένα λάθος όνομα χρήστη!';
+$lang['err_incorrect_username_pass'] = 'Το όνομα χρήστη και/ή ο κωδικός δεν είναι έγκυρα!';
+$lang['err_cid_not_found']           = 'Δεν βρέθηκε το ID της επαφής.<br><br>Παρακαλώ συνδεθείτε ξανά!';
+$lang['err_tid_not_found']           = 'Δεν βρέθηκε το ID του εισητηρίου.<br><br>Παρακαλώ συνδεθείτε ξανά!';
+$lang['err_no_ticket_found']         = 'Δεν βρέθηκαν εισητήρια!';
+$lang['err_no_contact_found']        = 'Δεν βρέθηκαν επαφές!';
+$lang['err_no_document_found']       = 'Δεν βρέθηκαν έγγραφα!';
+$lang['err_no_name']                 = 'Παρακαλώ γράψτε το όνομά σας!';
+$lang['err_no_subject']              = 'Παρακαλώ γράψτε ένα θέμα!';
+$lang['err_no_notes']                = 'Παρακαλώ γράψτε μια περιγραφή!';
+$lang['err_no_email']                = 'Παρακαλώ ενημερώστε το ηλεκτρονικό ταχυδρομείο σας!';
+$lang['err_not_supported_email']     = 'Παρακαλώ ενημερώστε μια έγκυρη διεύθυνση ηλεκτρονικού ταχυδρομείου!';
+$lang['err_no_responsibler_found']   = 'Δεν βρέθηκε το υπεύθυνο άτομο.<br><br>Παρακαλώ επικοινωνήστε με την υποστήριξη πελατών!';
+$lang['err_create_ticket']           = 'Σφάλμα στην δημιουργία εισητηρίου!<br><br>Παρακαλώ δοκιμάστε ξανά!';
+$lang['err_create_ticket_action']    = 'Σφάλμα την ενέργεια δημιουργίας εισητηρίου!<br><br>Παρακαλώ δοκιμάστε ξανά!';
+$lang['err_connection']              = 'Σφάλμα σύνδεσης! Παρακαλώ δοκιμάστε ξανά!';
+$lang['err_no_authorization']        = 'Δεν έχετε πρόσβαση σε αυτό το εισητήριο.<br><br>Παρακαλώ επικοινωνήστε με την υποστήριξη πελατών!';
+$lang['err_file_upload']             = 'Σφάλμα στην μεταφόρτωση αρχείου! Παρακαλώ δοκιμάστε ξανά.';
+$lang['err_adding_doc']              = 'Σφάλμα κατα την μεταφορά του εγγράφου στο σύστημα της TecArt!<br><br>Παρακαλώ δοκιμάστε ξανά!';
+$lang['err_create_folder']           = 'Σφάλμα στην δημιουργία νέου φακέλου!<br><br>Παρακαλώ δοκιμάστε ξανά!';
+$lang['err_db']                      = 'Σφάλμα κατα την παραλαβή του εγγράφου απο το σύστημα της TecArt!';
+$lang['no_activity']                 = 'Δεν υπάρχουν περιεχόμενα για τις επιλεγμένες δραστηριότητες!';
+$lang['no_set_activity']             = 'Παρακαλώ επιλέξτε τουλάχιστον μια δραστηριότητα!';
+$lang['err_filter_nomonth']          = 'Παρακαλώ επιλέξτε έτος πριν επιλέξετε φίλτρο μήνα.';
+$lang['err_ok']                      = 'Εντάξει';
+$lang['err_partners_not_enabled']    = 'Η λειτουργία της πύλης συνεργατών δεν είναι ενεργή.';
+$lang['err_partners_no_contract']    = 'Η πρόσβαση στην πύλη συνεργατών απορρίφθηκε! (μη ενεργό συμβόλαιο πρόσβασης συνεργατών ή επενεργοποιημένη πρόσβαση)';
+
+$lang['create_ticket_success']          = 'Το εισητήριο δημιουργήθικε επιτυχώς.';
+$lang['create_ticket_action_success']   = 'Η ενέργεια στάλθηκε επιτυχώς.';
+$lang['doc_upload_success']             = 'Το αρχείο μεταφορτώθηκε επιτυχώς.';
+$lang['change_pass_success']            = 'Ο κωδικός άλλαξε επιτυχώς.';
+$lang['change_pass_error']              = 'Σφάλμα κατα την επικαιροποίηση του κωδικού!';
+
+$lang['section_standard'] = 'Standard';
+
+$lang['levelup']      = 'Ενα επίπεδο πάνω';
+$lang['refresh']      = 'Επαναφόρτωση';
+$lang['folder_empty'] = 'Αυτός ο φάκελος ειναι άδειος.';
+$lang['mime_folder']  = 'Φάκελος';
